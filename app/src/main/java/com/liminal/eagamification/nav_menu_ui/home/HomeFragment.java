d93e4b5a584d9e27ac9991ff80458fd4656dd2b6
BhaktijPatil <bhaktij1999@gmail.com>
2020-03-20 14:05:18 +0530

    Added Unity Integration

--- app/src/main/java/com/liminal/eagamification/nav_menu_ui/home/HomeFragment.java
+++ app/src/main/java/com/liminal/eagamification/nav_menu_ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.liminal.eagamification.nav_menu_ui.home;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,6 +27,7 @@ import com.google.firebase.storage.UploadTask;
 import com.liminal.eagamification.EasyAugmentHelper;
 import com.liminal.eagamification.MenuActivity;
 import com.liminal.eagamification.R;
+import com.unity3d.player.UnityPlayerActivity;
 
 import java.io.File;
 
@@ -40,68 +43,68 @@ public class HomeFragment extends Fragment {
         EasyAugmentHelper easyAugmentHelper = new EasyAugmentHelper("101", getActivity(), MenuActivity.class.getName());
         easyAugmentHelper.loadMarkerImages();
 
-//        CardView arButton = root.findViewById(R.id.AR);
-//        CardView uploadButton = root.findViewById(R.id.Upload);
+        Button haikuButton = root.findViewById(R.id.gameStartButton1);
+        Button driveButton = root.findViewById(R.id.gameStartButton2);
 
+        haikuButton.setOnClickListener(v -> {
+            AlphaAnimation buttonClick = new AlphaAnimation(1f, 0.5f);
+            buttonClick.setDuration(100);
+            v.startAnimation(buttonClick);
+            new Handler().postDelayed(easyAugmentHelper::activateScanner,100);
+        });
 
-        Button arButton = root.findViewById(R.id.gameStartButton1);
-//        Button uploadButton = root.findViewById(R.id.uploadButton);
-
-//        uploadButton.setOnClickListener(view -> chooseNewImage());
-
-
-        arButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlphaAnimation buttonClick = new AlphaAnimation(1f, 0.5f);
-                buttonClick.setDuration(200);
-                v.startAnimation(buttonClick);
-                easyAugmentHelper.activateScanner();
-            }
+        driveButton.setOnClickListener(v -> {
+            AlphaAnimation buttonClick = new AlphaAnimation(1f, 0.5f);
+            buttonClick.setDuration(100);
+            v.startAnimation(buttonClick);
+            new Handler().postDelayed(() -> {
+                Intent unityIntent = new Intent(getActivity(), UnityPlayerActivity.class);
+                startActivity(unityIntent);
+            },100);
         });
 
         return root;
     }
 
-    void chooseNewImage() {
-        android.content.Intent intent = new android.content.Intent(android.content.Intent.ACTION_GET_CONTENT);
-        intent.addCategory(android.content.Intent.CATEGORY_OPENABLE);
-        intent.setType("image/*");
-        startActivityForResult(android.content.Intent.createChooser(intent, "Select target augmented image"),1);
-    }
-
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, android.content.Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        try {
-            if (resultCode == android.app.Activity.RESULT_OK) {
-                if (requestCode == 1)
-                {
-                    Uri imagePath = data.getData();
-//                    File file = new File(imagePath.getPath());
-                    StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("stor.jpg");
-
-                    storageReference.putFile(imagePath)
-                            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                                @Override
-                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                                    // Get a URL to the uploaded content
-
-//                                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception exception) {
-                                    // Handle unsuccessful uploads
-                                    // ...
-                                }
-                            });
-                }
-            }
-        } catch (Exception e) {
-            Log.e("EAG", "onActivityResult - target image selection error ", e);
-        }
-    }
+//    void chooseNewImage() {
+//        android.content.Intent intent = new android.content.Intent(android.content.Intent.ACTION_GET_CONTENT);
+//        intent.addCategory(android.content.Intent.CATEGORY_OPENABLE);
+//        intent.setType("image/*");
+//        startActivityForResult(android.content.Intent.createChooser(intent, "Select target augmented image"),1);
+//    }
+//
+//
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, android.content.Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        try {
+//            if (resultCode == android.app.Activity.RESULT_OK) {
+//                if (requestCode == 1)
+//                {
+//                    Uri imagePath = data.getData();
+////                    File file = new File(imagePath.getPath());
+//                    StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("stor.jpg");
+//
+//                    storageReference.putFile(imagePath)
+//                            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                                @Override
+//                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                                    // Get a URL to the uploaded content
+//
+////                                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+//                                }
+//                            })
+//                            .addOnFailureListener(new OnFailureListener() {
+//                                @Override
+//                                public void onFailure(@NonNull Exception exception) {
+//                                    // Handle unsuccessful uploads
+//                                    // ...
+//                                }
+//                            });
+//                }
+//            }
+//        } catch (Exception e) {
+//            Log.e("EAG", "onActivityResult - target image selection error ", e);
+//        }
+//    }
 }
