a4e482aefb960789c632eaeb2001024e58421f4c
BhaktijPatil <bhaktij1999@gmail.com>
2020-04-12 16:33:52 +0530

    Functionality added to user profile

--- app/src/main/java/com/liminal/eagamification/UserProfile.java
+++ app/src/main/java/com/liminal/eagamification/UserProfile.java
@@ -7,15 +7,18 @@ import com.google.firebase.database.IgnoreExtraProperties;
 import java.net.URI;
 
 @IgnoreExtraProperties
-class UserProfile {
+public class UserProfile {
 
     // User details
     String ID;
 
+    public String userName;
     public String email;
     public String firstName;
     public String lastName;
     public String photoURL;
+    public String phoneNo;
+    public String DOB;
 
     // Empty constructor needed for Firebase
     UserProfile()
@@ -23,13 +26,27 @@ class UserProfile {
 
     }
 
-    UserProfile(String email, String firstName, String lastName, Uri photoURL, String ID)
+    public UserProfile(String email, String firstName, String lastName, Uri photoURL, String ID)
     {
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
         this.photoURL = String.valueOf(photoURL);
         this.ID = ID;
+        this.phoneNo = "NA";
+        this.DOB = "NA";
+        this.userName = "NA";
+    }
+
+    public UserProfile(String email, String firstName, String lastName, String photoURL, String ID, String phoneNo, String DOB, String userName)
+    {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.photoURL = String.valueOf(photoURL);
+        this.ID = ID;
+        this.phoneNo = phoneNo;
+        this.DOB = DOB;
+        this.userName = userName;
     }
 }
-
--- app/src/main/java/com/liminal/eagamification/nav_menu_ui/profile/ProfileFragment.java
+++ app/src/main/java/com/liminal/eagamification/nav_menu_ui/profile/ProfileFragment.java
@@ -2,18 +2,228 @@ package com.liminal.eagamification.nav_menu_ui.profile;
 
 
 import androidx.fragment.app.Fragment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.FirebaseError;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.liminal.eagamification.MenuActivity;
 import com.liminal.eagamification.R;
+import com.liminal.eagamification.UserProfile;
+
+import java.io.File;
+import java.net.URI;
 
 public class ProfileFragment extends Fragment {
+
+    private TextView userNameTextView;
+
+    private EditText userNameEditText;
+    private EditText firstNameEditText;
+    private EditText lastNameEditText;
+    private EditText phoneNoEditText;
+    private EditText DOBEditText;
+
+    private ImageView profilePictureView;
+
+    private SharedPreferences sharedPreferences;
+    private StorageReference profilePictureRef;
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // Get shared references for user's ID and email
+        sharedPreferences = getActivity().getSharedPreferences("User_Details", Context.MODE_PRIVATE);
+
+        // Create a reference to all profile fields
+        userNameTextView = root.findViewById(R.id.userNameTextView);
+
+        userNameEditText = root.findViewById(R.id.userNameEditText);
+        firstNameEditText = root.findViewById(R.id.firstNameEditText);
+        lastNameEditText = root.findViewById(R.id.lastNameEditText);
+        phoneNoEditText = root.findViewById(R.id.phoneEditText);
+        DOBEditText = root.findViewById(R.id.dobEditText);
+
+        profilePictureView = root.findViewById(R.id.profilePictureview);
+
+        Button changePicButton = root.findViewById(R.id.addPicButton);
+        Button updateProfileButton = root.findViewById(R.id.updateProfileButton);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("userProfileTable");
+        profilePictureRef = FirebaseStorage.getInstance().getReference().child(sharedPreferences.getString("id","")).child("ProfilePicture");
+
+        // Load the current profile picture into imageview
+        loadProfilePicture();
+
+        // Add a listener to update UI when User Profile is updated
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Read data from firebase and update EditProfile layout
+                updateUserProfileLayout(root,
+                        (String) dataSnapshot.child("personalDetails").child("userName").getValue(),
+                        (String) dataSnapshot.child("personalDetails").child("firstName").getValue(),
+                        (String) dataSnapshot.child("personalDetails").child("lastName").getValue(),
+                        (String) dataSnapshot.child("personalDetails").child("DOB").getValue(),
+                        (String) dataSnapshot.child("personalDetails").child("phoneNo").getValue());
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Read failed
+                Log.d("EAG_FIREBASE_DB", "Failed to read data from Firebase : ", databaseError.toException());
+            }
+        };
+
+        databaseReference.child(sharedPreferences.getString("id","")).addValueEventListener(eventListener);
+
+        // Upload profile picture
+        changePicButton.setOnClickListener(v -> chooseNewImage());
+
+        // Update profile information
+        updateProfileButton.setOnClickListener(v -> updateUserProfile());
+
         return root;
+    }
+
+    // Function to load profile picture
+    private void loadProfilePicture()
+    {
+        Log.d("EAG_DP_LOAD", "Loading Picture : " + profilePictureRef.getName());
+        Glide.with(getActivity())
+                .load(profilePictureRef)
+                .into(profilePictureView);
+    }
+
+    // Function to update Edit Profile Layout with current field values
+    private void updateUserProfileLayout(View root, String userName, String firstName, String lastName, String dob, String phoneNo)
+    {
+        // Set Text fields for all Edit texts
+        if(!userName.equals("NA"))
+        {
+            userNameTextView.setText(userName);
+            userNameEditText.setText(userName);
+        }
+
+        firstNameEditText.setText(firstName);
+        lastNameEditText.setText(lastName);
+
+        if(!dob.equals("NA"))
+            DOBEditText.setText(dob);
+
+        if(!phoneNo.equals("NA"))
+            phoneNoEditText.setText(phoneNo);
+
+        Log.d("EAG_EDIT_PROFILE", "Username : " + userName + " First Name : " + firstName + " Last Name : " + lastName +  " DOB : " + dob + " Mobile No : " + phoneNo);
+    }
+
+    // Function to choose new image
+    private void chooseNewImage()
+    {
+        android.content.Intent intent = new android.content.Intent(android.content.Intent.ACTION_GET_CONTENT);
+        intent.addCategory(android.content.Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(android.content.Intent.createChooser(intent, "Select target augmented image"), 1);
+    }
+
+    // Get result of choose image here
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, android.content.Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (resultCode == android.app.Activity.RESULT_OK) {
+                if (requestCode == 1) {
+                    // Get the URI of target image
+                    Uri imageURI = data.getData();
+                    // Upload to firebase
+                    profilePictureRef.putFile(imageURI);
+                }
+            }
+        } catch (Exception e) {
+            Log.e("EAG_CHOOSE_IMAGE", "Target image selection error ", e);
+        }
+    }
+
+    // Function to upload new user details on Firebase
+    private void updateUserProfile()
+    {
+        String userName = String.valueOf(userNameEditText.getText()).equals("") ? "NA" : String.valueOf(userNameEditText.getText());
+        String firstName = String.valueOf(firstNameEditText.getText());
+        String lastName = String.valueOf(lastNameEditText.getText());
+        String DOB = String.valueOf(DOBEditText.getText()).equals("") ? "NA" : String.valueOf(DOBEditText.getText());
+        String phoneNo = String.valueOf(phoneNoEditText.getText()).equals("") ? "NA" : String.valueOf(phoneNoEditText.getText());
+
+        UserProfile userProfile = new UserProfile(
+                sharedPreferences.getString("email", ""),
+                firstName,
+                lastName,
+                sharedPreferences.getString("photo_url", ""),
+                sharedPreferences.getString("id", ""),
+                phoneNo,
+                DOB,
+                userName);
+
+        if(checkUsernameValidity(userName))
+        {
+            DatabaseReference userNameDatabaseReference = FirebaseDatabase.getInstance().getReference().child("userNameTable");
+
+            userNameDatabaseReference.orderByChild("userName").equalTo(userName).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // username is unique
+                    if (!dataSnapshot.exists()) {
+                        DatabaseReference userProfileDatabaseReference = FirebaseDatabase.getInstance().getReference().child("userProfileTable");
+                        userProfileDatabaseReference.child(sharedPreferences.getString("id", "")).child("personalDetails").setValue(userProfile);
+                        userNameDatabaseReference.child(sharedPreferences.getString("id", "")).child("userName").setValue(userName);
+                    }
+                    else
+                    {
+                        Toast.makeText(getActivity(), "Username already exists", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }});
+        }
+    }
+
+    // Function to check validity of username
+    private boolean checkUsernameValidity(String text) {
+        // Set character limit of 4 for username
+        if (text.length() < 4)
+        {
+            Toast.makeText(getActivity(), "Username must be atleast 4 letters long", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        // Only allow alpha numeric characters and underscores
+        if (!text.matches("^[a-zA-Z0-9_]*$"))
+        {
+            Toast.makeText(getActivity(), "Username cannot contain special characters", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 }
